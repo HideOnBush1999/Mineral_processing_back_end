ea3090b8a2083148e25b9e64fab8b40176266cf3
cheng <1240800450@qq.com>
2024-03-13 22:46:54 +0800

    feat: add neo4j_driver

--- utils/database.py
+++ utils/database.py
@@ -1,24 +1,37 @@
-# database.py
 import pymysql
 from dbutils.pooled_db import PooledDB
+from neo4j import GraphDatabase
 
-db_config = {
+# MySQL database configuration
+mysql_db_config = {
     'host': '127.0.0.1',
     'user': 'root',
     'password': '123456',
     'database': 'mine'
 }
 
-
-def create_db_pool():
+# Create MySQL database pool
+def create_mysql_db_pool():
     return PooledDB(
         pymysql,
         mincached=5,
         maxcached=10,
         maxconnections=20,
         blocking=True,
-        **db_config
+        **mysql_db_config
     )
 
+# Neo4j database credentials
+neo4j_uri = "bolt://localhost:7687"
+neo4j_username = "neo4j"
+neo4j_password = "password"
 
-db_pool = create_db_pool()
+# Connect to Neo4j database
+def create_neo4j_driver():
+    return GraphDatabase.driver(neo4j_uri, auth=(neo4j_username, neo4j_password))
+
+# MySQL database pool instance
+mysql_db_pool = create_mysql_db_pool()
+
+# Neo4j database driver instance
+neo4j_driver = create_neo4j_driver()
