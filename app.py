46a93ab00ab4475c1b62f65af48a4e849d0fe978
cheng <1240800450@qq.com>
2024-03-14 20:52:16 +0800

    feat: add new qa/chat api

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from blueprints.login import login
 from blueprints.qa_system import qa
 from blueprints.multi_layer_modeling import multi_layer_modeling
 from blueprints.correlation_analysis import correlation_analysis
+# from xinference.client import Client
 
 app = Flask(__name__)
 CORS(app)
@@ -15,6 +16,14 @@ app.register_blueprint(qa)
 app.register_blueprint(multi_layer_modeling)
 app.register_blueprint(correlation_analysis)
 
+# model = None
+
+# @app.before_first_request
+# def load_models():
+#     global model
+#     client = Client("http://localhost:9997")
+#     model_uid = client.launch_model(model_name="local-qwen-7b-q5_k_m")
+#     model = client.get_model(model_uid)
 
 @app.route('/hello', methods=['GET'])
 def hello():
@@ -24,4 +33,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5005, debug=True)
+    app.run(host='127.0.0.1', port=5005, debug=False)
--- blueprints/login.py
+++ blueprints/login.py
@@ -1,20 +1,20 @@
 from flask import Blueprint, request, jsonify
 from utils.encryption import verify_password
-from utils.database import db_pool
+from utils.database import mysql_db_pool
 from utils.logger import logger
 
 
 login = Blueprint('login', __name__, url_prefix='/login')
 
 
-@login.route('/', methods=['POST'])
-def login():
+@login.route('', methods=['POST'])
+def logining():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
 
     try:
-        with db_pool.connection() as conn:   # 使用数据库连接池中的连接
+        with mysql_db_pool.connection() as conn:   # 使用数据库连接池中的连接
             with conn.cursor() as cursor:    # 连接上创建游标
                 query = "SELECT password, salt FROM users WHERE username = %s"   # 查询语句
                 cursor.execute(query, (username,))  # 执行查询语句
