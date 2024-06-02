2462927a56ee4c02cb33bb5dd8073a4be41e0f4e
cheng <1240800450@qq.com>
2024-06-02 20:37:56 +0800

    feat: add minio

--- utils/database.py
+++ utils/database.py
@@ -1,10 +1,12 @@
 import pymysql
 from dbutils.pooled_db import PooledDB
 from neo4j import GraphDatabase
+from minio import Minio
 
 # 全局变量
 mysql_db_pool = None
 neo4j_driver = None
+minio_client = None
 
 # MySQL database configuration
 mysql_db_config = {
@@ -40,4 +42,24 @@ def get_neo4j_driver():
         neo4j_driver = GraphDatabase.driver(
             neo4j_uri, auth=(neo4j_username, neo4j_password))
     return neo4j_driver
+
+# MinIO client configuration
+minio_config = {
+    'endpoint': '127.0.0.1:9000',
+    'access_key': 'minioadmin',
+    'secret_key': 'minioadmin',
+    'secure': False  # 如果使用的是http而不是https，设为False
+}
+
+# Create MinIO client
+def get_minio_client():
+    global minio_client
+    if minio_client is None:
+        minio_client = Minio(
+            minio_config['endpoint'],
+            access_key=minio_config['access_key'],
+            secret_key=minio_config['secret_key'],
+            secure=minio_config['secure']
+        )
+    return minio_client
 
