1389c0e8fbdf31cdb682df59e1ceb57a5eb31a70
cheng <1240800450@qq.com>
2024-06-22 19:47:00 +0800

    feat: register multi_layer_manage blueprint

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from blueprints.login import login
 from blueprints.chat import qa
 from blueprints.traid import traid
 from blueprints.multi_layer_modeling import multi_layer_modeling
+from blueprints.multi_layer_manage import multi_layer_manage
 from blueprints.parameter_extraction import parameter_extraction
 from blueprints.correlation_analysis import correlation_analysis
 from utils.websocket import socketio
@@ -14,12 +15,13 @@ socketio.init_app(app, cors_allowed_origins="*")
 
 
 app.register_blueprint(login)
+app.register_blueprint(multi_layer_modeling)
+app.register_blueprint(multi_layer_manage)
 app.register_blueprint(qa)
 app.register_blueprint(traid)
-app.register_blueprint(multi_layer_modeling)
 app.register_blueprint(parameter_extraction)
 app.register_blueprint(correlation_analysis)
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='127.0.0.1', port=5005, debug=False)
+    socketio.run(app, host='127.0.0.1', port=5005, debug=True)
