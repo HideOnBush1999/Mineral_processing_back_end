7ab90112f97e7ae472afa5449fdacbf953b4d9c1
cheng <1240800450@qq.com>
2024-06-28 20:00:45 +0800

    feat: add celery

---
+++ utils/celery.py
@@ -0,0 +1,70 @@
+from celery import Celery
+from utils.optimization import get_optimization_results, get_model, get_bounds
+from utils.logger import logger
+from utils.websocket import socketio
+
+
+
+class CelerySingleton:
+    _instance = None
+
+    def __new__(cls, config=None):
+        if cls._instance is None:
+            if config is None:
+                raise ValueError("A config dictionary must be provided for the first initialization.")
+            cls._instance = cls._create_celery(config)
+        return cls._instance
+
+    @staticmethod
+    def _create_celery(config):
+        celery = Celery(
+            config['app_name'],
+            backend=config['result_backend'],  # 使用新格式
+            broker=config['broker_url']        # 使用新格式
+        )
+        celery.conf.update(config)  # 确保其他配置也是使用新格式
+        celery.conf.task_time_limit = 3600  # 设置任务时间限制为 3600 秒
+        celery.autodiscover_tasks(['utils.celery'])  # 自动发现任务
+        return celery
+
+def make_celery(config):
+    return CelerySingleton(config)
+
+# 配置变量，使用新格式
+celery_config = {
+    'app_name': 'my_app',
+    'broker_url': 'redis://localhost:6379/0',  # 新格式
+    'result_backend': 'redis://localhost:6379/0',  # 新格式
+    'broker_connection_retry_on_startup': True,
+}
+
+# 获取 Celery 实例
+celery = make_celery(celery_config)
+
+
+@celery.task(bind=True)
+def optimize_task(self, dataset_name, module_name, model_name):
+    try:
+        print("开始优化任务")
+        logger.info(f"开始优化任务: dataset_name={dataset_name}, module_name={module_name}, model_name={model_name}")
+        model = get_model(model_name)
+        bounds = get_bounds(dataset_name, module_name)
+        optimal_inputs, optimal_value = get_optimization_results(dataset_name, module_name, model, bounds, particles=1, iterations=1)
+        result = {'optimal_inputs': optimal_inputs.tolist(), 'optimal_value': optimal_value.tolist()}
+        logger.info("优化任务完成")
+
+        # 任务完成后发送WebSocket事件
+        logger.info("123456")
+        socketio.emit('optimization_result', result, namespace='/multi_layer_modeling')
+        logger.info("WebSocket事件发送成功")
+        return result
+
+    except Exception as e:
+        logger.error(f"优化任务出错: {e}")
+        error_result = {'state': 'FAILURE', 'error': str(e)}
+        
+        # 任务失败后发送WebSocket事件
+        logger.info("abcde")
+        socketio.emit('optimization_result', error_result, namespace='/multi_layer_modeling')
+        logger.info("WebSocket事件发送成功")
+        return error_result
