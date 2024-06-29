a7c02f08e48d3b7cf8ec7d664acfd957e218f9a8
cheng <1240800450@qq.com>
2024-06-29 21:52:46 +0800

    feat: modify optimization_solve & add get_optimization_result api

--- blueprints/multi_layer_modeling.py
+++ blueprints/multi_layer_modeling.py
@@ -1,19 +1,20 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, current_app
 from minio.error import S3Error
 import os
 from utils.database import get_minio_client
-import numpy as np
 import pandas as pd
 import io
 from sklearn.ensemble import RandomForestRegressor
 import joblib
 from datetime import datetime
 from sklearn.metrics import mean_squared_error, r2_score
-from scipy.interpolate import interp1d
 from sklearn.model_selection import train_test_split
 from utils.logger import logger
-from utils.optimization import get_optimization_results
-
+from utils.celery import optimize_task, celery
+from utils.websocket import socketio
+from flask_socketio import emit    # 使用 emit 是不行的
+from utils.optimization import get_model, interpolate_data
+import json
 
 
 multi_layer_modeling = Blueprint(
@@ -141,46 +142,113 @@ def get_model_list():
         return jsonify({'error': f'An error occurred during get model list: {str(e)}'}), 500
 
 
-# 优化求解  (请求：数据集名， 模块名， 模型名)
+# # 优化求解  (请求：数据集名， 模块名， 模型名)  最初版本，不使用celery
+# @multi_layer_modeling.route('/optimization_solve', methods=['POST'])
+# def optimization_solve():
+#     try:
+#         # 获取请求参数
+#         data = request.get_json()
+#         dataset_name = data.get('dataset_name')
+#         module_name = data.get('module_name')
+#         model_name = data.get('model_name')
+
+#         model = get_model(model_name)
+
+#         # 得到对应参数的上下限
+#         bounds = get_bounds(dataset_name, module_name)
+
+#         # 得到求解的结果
+#         optimal_inputs, optimal_value = get_optimization_results(dataset_name, module_name, model, bounds, particles=10, iterations=2)
+        
+#         # 将 NumPy 数组转换为 Python 列表
+#         optimal_inputs_list = optimal_inputs.tolist()
+#         optimal_value_list = optimal_value.tolist()
+
+#         logger.info(f"optimal_inputs: {optimal_inputs_list}, optimal_value: {optimal_value_list}".format())
+#         return jsonify({'optimal_inputs': optimal_inputs_list, 'optimal_value': optimal_value_list}), 200
+
+#     except Exception as e:
+#         return jsonify({'error': f'An error occurred during optimization: {str(e)}'}), 500
+    
+
+
+# 优化求解  (请求：数据集名， 模块名， 模型名)  使用celery
 @multi_layer_modeling.route('/optimization_solve', methods=['POST'])
 def optimization_solve():
+    socketio.emit('optimization_result', {'state': 'running', 'info': '优化任务正在运行'})
     try:
-        # 获取请求参数
         data = request.get_json()
         dataset_name = data.get('dataset_name')
         module_name = data.get('module_name')
         model_name = data.get('model_name')
 
-        model = get_model(model_name)
-
-        # 得到对应参数的上下限
-        bounds = get_bounds(dataset_name, module_name)
-
-        # 得到求解的结果
-        optimal_inputs, optimal_value = get_optimization_results(dataset_name, module_name, model, bounds, particles=30, iterations=2)
-        
-        # 将 NumPy 数组转换为 Python 列表
-        optimal_inputs_list = optimal_inputs.tolist()
-        optimal_value_list = optimal_value.tolist()
-
-        logger.info(f"optimal_inputs: {optimal_inputs_list}, optimal_value: {optimal_value_list}".format())
-        return jsonify({'optimal_inputs': optimal_inputs_list, 'optimal_value': optimal_value_list}), 200
+        task = optimize_task.apply_async(args=[dataset_name, module_name, model_name])
+        return jsonify({'task_id': task.id}), 202   # 202 表示服务器已接受请求，但尚未处理完毕
 
     except Exception as e:
-        return jsonify({'error': f'An error occurred during optimization: {str(e)}'}), 500
+        return jsonify({'error': f'优化过程中发生错误: {str(e)}'}), 500
 
 
-def interpolate_data(df, num_points):
-    x_old = np.arange(len(df))
-    x_new = np.linspace(0, len(df) - 1, num=num_points)
-    df_interpolated = pd.DataFrame()
+# 这是暴露了一个 websocket 接口，需要前端主动请求
+@socketio.on('get_optimization_result', namespace='/multi_layer_modeling')
+def get_optimization_result(message):
+    logger.info(f"前端请求获取优化结果：{message}")
+    message = json.loads(message)
+    task_id = message['task_id']
+    task = optimize_task.AsyncResult(task_id)
+    if task.state == 'SUCCESS':
+        logger.info(f"优化结果：{task.result}")
+        emit('optimization_result', task.result, namespace='/multi_layer_modeling')
+    elif task.state == 'FAILURE':
+        logger.info(f"优化任务失败，任务 ID：{task_id} 状态：{task.state}")
+        emit('optimization_result', {'state': task.state, 'error': str(task.info)}, namespace='/multi_layer_modeling')
+    else:
+        logger.info(f"优化任务进行中，任务 ID：{task_id} 状态：{task.state}")
+        emit('optimization_result', {'state': task.state, 'info': task.info}, namespace='/multi_layer_modeling')
 
-    for col in df.columns:
-        f = interp1d(x_old, df[col], kind='quadratic',
-                     fill_value="extrapolate")
-        df_interpolated[col] = f(x_new)
 
-    return df_interpolated
+# 获取优化结果  (请求：任务 ID)
+@multi_layer_modeling.route('/optimization_result/<task_id>', methods=['GET'])
+def get_optimization_result(task_id):
+    try:
+        print("获取优化结果")
+        # 获取任务异步结果对象
+        task_result = celery.AsyncResult(task_id)
+        
+        # 检查任务状态
+        if task_result.state == 'PENDING':
+            response = {
+                'state': task_result.state,
+                'status': '任务尚未开始或正在等待执行'
+            }
+        elif task_result.state == 'PROGRESS':
+            response = {
+                'state': task_result.state,
+                'status': '任务正在执行'
+            }
+        elif task_result.state == 'SUCCESS':
+            response = {
+                'state': task_result.state,
+                'result': task_result.result,
+                'status': '任务已完成'
+            }
+        elif task_result.state == 'FAILURE':
+            response = {
+                'state': task_result.state,
+                'status': '任务失败',
+                'result': str(task_result.info)  # 任务失败原因
+            }
+        else:
+            response = {
+                'state': task_result.state,
+                'status': '未知状态'
+            }
+        
+        return jsonify(response), 200
+
+    except Exception as e:
+        return jsonify({'error': f'获取结果时发生错误: {str(e)}'}), 500
+
 
 
 def data_split(dataset_name, module_name):
@@ -281,83 +349,3 @@ def train_online(X_train, y_train, module_name, tree_num):
         model_path
     )
 
-
-def get_bounds(dataset_name, module_name):
-    # 读取数据集
-    local_dir = './data/multi_layer'
-    dataset_path = os.path.join(local_dir, dataset_name)
-    logger.info("读取数据集：{}".format(dataset_path))
-    try:
-        data = pd.read_excel(dataset_path)
-    except Exception as e:
-        logger.error("数据集读取失败：{}".format(e))
-
-    if module_name == "给煤机" or module_name == "给风机" or module_name == "磨煤":
-        data = data.drop(data.index[:50])
-        if module_name == "给煤机":
-            input_cols = ['皮带转速', '比例系数']
-
-        if module_name == "给风机":
-            input_cols = ['热风阀门开度', '冷风阀门开度', '热一次风温度', '冷一次风温度']
-
-        if module_name == "磨煤":
-            input_cols = ['入口一次风流量', '入口一次风温度', '给煤量', '磨煤机电流', '原煤温度', '原煤水分']
-
-    if module_name == "锅炉进口空预器" or module_name == "给水系统" or module_name == "锅炉燃烧":
-        data = data.dropna()
-        num_samples = 1000
-        data = interpolate_data(data, num_samples)
-
-        if module_name == "锅炉进口空预器":
-            input_cols = ['O2 in APH (%)', 'Flue Gas in Temperature (°C)',
-                          'Flue gas temperature (℃)']
-
-        if module_name == "给水系统":
-            input_cols = [
-                'Superheater desuperheating water flow (t/h)',
-                'Reheater desuperheating water flow (t/h)',
-                'Feedwater pressure (MPa)',
-                'Flue gas temperature (℃)',
-                'Circulating water outlet temperature (℃)'
-            ]
-
-        if module_name == "锅炉燃烧":
-            input_cols = [
-                'Coal Flow (t/h)',
-                'O2 Out APH (%)',
-                'Corrected Flue Gas Out Temperature (°C)',
-                'Feedwater temperature (℃)',
-                'Feedwater flow (t/h)',
-                'Energy Input From Boiler (Kcal/h)',
-                'Boiler oxygen level (%)'
-            ]
-
-    input_bounds = []
-    for col in input_cols:
-        input_bounds.append((data[col].min(), data[col].max()))
-
-    bounds = tuple(input_bounds)
-    logger.info("输入参数上下限：{}".format(bounds))
-    return bounds
-
-
-def get_model(model_name):
-    # 读取模型
-    model_dir = './model/multi-layer-model'
-    model_path = os.path.join(model_dir, model_name)
-
-    # 如果本地不存在，则去 MinIO 下载
-    if not os.path.exists(model_path):
-        minio_client = get_minio_client()
-        bucket_name = 'multi-layer-model'
-
-        minio_client.fget_object(
-            bucket_name,
-            model_name,
-            model_path
-        )
-
-    # 读取模型
-    model = joblib.load(model_path)
-    logger.info(f'Loaded model {model_name} from {model_path}')
-    return model
